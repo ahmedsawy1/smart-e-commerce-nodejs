a4c669244975ca02fa7cdc0c1ee6057a7e7633d5
Ahmed Al Sawy <sawy22417@gmail.com>
2021-12-08 16:36:24 +0200

    update password feature

--- routes/users/update-profile.js
+++ routes/users/update-profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { UserModel } = require("../../model/user");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 router.put("/edit", async (req, res) => {
   const token = req.header("authorization").substring(7);
@@ -20,6 +21,27 @@ router.put("/edit", async (req, res) => {
   );
 
   res.send(updatedUser);
+});
+
+router.put("/edit/password", async (req, res) => {
+  const token = req.header("authorization").substring(7);
+  const decodedToken = jwt.decode(token, { complete: true });
+  const userId = decodedToken.payload.userId;
+  const user = await UserModel.findById(userId);
+  //   v1@gmail
+  if (user && bcrypt.compareSync(req.body.oldPassword, user.passwordHash)) {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      {
+        passwordHash: bcrypt.hashSync(req.body.newPassword, 10),
+      },
+      { new: true }
+    );
+
+    res.send(updatedUser);
+  } else {
+    res.send("Password Is Not Correct");
+  }
 });
 
 module.exports = router;
