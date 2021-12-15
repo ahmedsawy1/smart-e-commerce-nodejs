66ac7cb024053300bd13aeb87bf57eb53a245ae0
Ahmed Al Sawy <sawy22417@gmail.com>
2021-12-15 11:16:20 +0200

    Debugging

--- routes/users/users.js
+++ routes/users/users.js
@@ -15,77 +15,77 @@ router.get("/", async (req, res) => {
   res.send(userList);
 });
 
-router.post("/register", async (req, res) => {
-  let newUser = new UserModel({
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    passwordHash: bcrypt.hashSync(req.body.password, 10),
-    country: req.body.country,
-    city: req.body.city,
-  });
+// router.post("/register", async (req, res) => {
+//   let newUser = new UserModel({
+//     name: req.body.name,
+//     email: req.body.email,
+//     phone: req.body.phone,
+//     passwordHash: bcrypt.hashSync(req.body.password, 10),
+//     country: req.body.country,
+//     city: req.body.city,
+//   });
 
-  newUser = await newUser.save();
+//   newUser = await newUser.save();
 
-  if (!newUser) return res.status(400).send("The user cannot be created!");
+//   if (!newUser) return res.status(400).send("The user cannot be created!");
 
-  res.send(newUser);
-});
+//   res.send(newUser);
+// });
 
-router.post("/login", async (req, res) => {
-  const user = await UserModel.findOne({ email: req.body.email });
-  if (!user) {
-    return res.status(400).send("User Is Not Found");
-  }
+// router.post("/login", async (req, res) => {
+//   const user = await UserModel.findOne({ email: req.body.email });
+//   if (!user) {
+//     return res.status(400).send("User Is Not Found");
+//   }
 
-  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-    const token = jwt.sign(
-      {
-        userId: user.id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.SECRET,
-      {
-        expiresIn: "50d", // >> on day
-      }
-    );
-    res.status(200).send({
-      user: user.email,
-      token: token,
-    });
-  } else {
-    return res.status(400).send("Password is wrong");
-  }
-});
+//   if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+//     const token = jwt.sign(
+//       {
+//         userId: user.id,
+//         isAdmin: user.isAdmin,
+//       },
+//       process.env.SECRET,
+//       {
+//         expiresIn: "50d", // >> on day
+//       }
+//     );
+//     res.status(200).send({
+//       user: user.email,
+//       token: token,
+//     });
+//   } else {
+//     return res.status(400).send("Password is wrong");
+//   }
+// });
 
-router.put("/addToFav/:prodId", async (req, res) => {
-  const token = req.header("authorization").substring(7);
-  const decodedToken = jwt.decode(token, { complete: true });
-  const userId = decodedToken.payload.userId;
-  console.log(userId);
+// router.put("/addToFav/:prodId", async (req, res) => {
+//   const token = req.header("authorization").substring(7);
+//   const decodedToken = jwt.decode(token, { complete: true });
+//   const userId = decodedToken.payload.userId;
+//   console.log(userId);
 
-  const products = await ProductModel.findById(req.params.prodId);
-  const User = await UserModel.findById(userId).populate("favs");
+//   const products = await ProductModel.findById(req.params.prodId);
+//   const User = await UserModel.findById(userId).populate("favs");
 
-  const favs = () => {
-    if (User.favs.some((Product) => Product._id == req.params.prodId)) {
-      return User.favs;
-    }
+//   const favs = () => {
+//     if (User.favs.some((Product) => Product._id == req.params.prodId)) {
+//       return User.favs;
+//     }
 
-    return [...User.favs, products._id];
-  };
+//     return [...User.favs, products._id];
+//   };
 
-  const favArr = favs();
+//   const favArr = favs();
 
-  const updatedUser = await UserModel.findByIdAndUpdate(
-    userId,
-    {
-      favs: favArr,
-    },
-    { new: true }
-  ).populate("favs");
+//   const updatedUser = await UserModel.findByIdAndUpdate(
+//     userId,
+//     {
+//       favs: favArr,
+//     },
+//     { new: true }
+//   ).populate("favs");
 
-  res.send(updatedUser);
-});
+//   res.send(updatedUser);
+// });
 
 module.exports = router;
