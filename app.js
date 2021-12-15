ca0e91b4c8c836bd32d26e79e648bf0fda2eadff
Ahmed Al Sawy <sawy22417@gmail.com>
2021-12-15 15:29:54 +0200

    Deploy to heroku

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const catgsRouter = require("./routes/catgs");
 const productsRouter = require("./routes/products");
 const addressRouter = require("./routes/users/addresses");
 const updateProfile = require("./routes/users/update-profile");
-// const resetPassword = require("./routes/users/user-reset-password");
-// const ordersRouter = require("./routes/orders/orders");
-// const shipingRouter = require("./routes/orders/shiping");
-// const paymentRouter = require("./routes/orders/payment");
+const resetPassword = require("./routes/users/user-reset-password");
+const ordersRouter = require("./routes/orders/orders");
+const shipingRouter = require("./routes/orders/shiping");
+const paymentRouter = require("./routes/orders/payment");
 
 const auth = require("./helpers/jwt");
 
@@ -40,13 +40,13 @@ app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
 app.use(`${api}/test`, testRouter);
 app.use(`${api}/users`, usersRouter);
-// app.use(`${api}/users`, resetPassword);
+app.use(`${api}/users`, resetPassword);
 app.use(`${api}/users`, updateProfile);
 app.use(`${api}/catgs`, catgsRouter);
 app.use(`${api}/products`, productsRouter);
-// app.use(`${api}/orders`, ordersRouter);
-// app.use(`${api}/shiping`, shipingRouter);
-// app.use(`${api}/payment`, paymentRouter);
+app.use(`${api}/orders`, ordersRouter);
+app.use(`${api}/shiping`, shipingRouter);
+app.use(`${api}/payment`, paymentRouter);
 app.use(`${api}/address`, addressRouter);
 
 mongoose
