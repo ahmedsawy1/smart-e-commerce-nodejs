59652ff8d768738f10183dcb5570b775bb76ec24
Ahmed Al Sawy <sawy22417@gmail.com>
2021-12-15 13:51:18 +0200

    debugging

--- routes/users/update-profile.js
+++ routes/users/update-profile.js
@@ -23,24 +23,24 @@ router.put("/edit", async (req, res) => {
   res.send(updatedUser);
 });
 
-// router.put("/edit/password", async (req, res) => {
-//   const token = req.header("authorization").substring(7);
-//   const decodedToken = jwt.decode(token, { complete: true });
-//   const userId = decodedToken.payload.userId;
-//   const user = await UserModel.findById(userId);
-//   if (user && bcrypt.compareSync(req.body.oldPassword, user.passwordHash)) {
-//     const updatedUser = await UserModel.findByIdAndUpdate(
-//       userId,
-//       {
-//         passwordHash: bcrypt.hashSync(req.body.newPassword, 10),
-//       },
-//       { new: true }
-//     );
+router.put("/edit/password", async (req, res) => {
+  const token = req.header("authorization").substring(7);
+  const decodedToken = jwt.decode(token, { complete: true });
+  const userId = decodedToken.payload.userId;
+  const user = await UserModel.findById(userId);
+  if (user && bcrypt.compareSync(req.body.oldPassword, user.passwordHash)) {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      {
+        passwordHash: bcrypt.hashSync(req.body.newPassword, 10),
+      },
+      { new: true }
+    );
 
-//     res.send(updatedUser);
-//   } else {
-//     res.send("Password Is Not Correct");
-//   }
-// });
+    res.send(updatedUser);
+  } else {
+    res.send("Password Is Not Correct");
+  }
+});
 
 module.exports = router;
